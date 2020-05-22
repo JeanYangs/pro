658bf8bd8d677616193ffcf701ff9af3c5f849ba
vclass <vclass@vclass.com>
2020-05-22 23:00:27 +0800

    all

---
+++ src/main/java/com/vclass/App.java
@@ -0,0 +1,19 @@
+package com.vclass;
+
+import com.vclass.web.Computer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+{
+    System.out.println( "Hello World!" );
+    Computer com = new Computer();
+    com.sysOut();
+    System.out.println("dev2");
+
+}
+}
